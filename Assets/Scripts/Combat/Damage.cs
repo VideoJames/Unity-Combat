352ffc27a2cd2cf0413c571b13df97842e03c17b
VideoJames <james@metia.co.nz>
2018-08-22 22:45:12 +1200

    Tried to start on combat. Not sure where I'm going right now.

---
+++ Assets/Scripts/Combat/Damage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Damage
+{
+    int BaseValue { get; }
+    int CurrentValue { get; }
+    void ApplyDamage(DamageReceiver damageReceiver);
+}
+
+public interface DamageReceiver
+{
+    void ReceiveDamage(Damage damage);
+}
+
+public interface DamageMediator
+{
+
+}
---
+++ Assets/Scripts/Combat/Effect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burn : ScriptableObject , Effect
+{
+
+}
+public interface Effect
+{
+
+	
+}
+public class TimedEffect : MonoBehaviour
+{
+
+}
